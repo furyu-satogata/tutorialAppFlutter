3e924eb0254226dc553d13d52454e2fbceb38ffe
furyu-satogata <satogata.hiromichi@furyu.jp>
2022-11-09 14:43:19 +0900

    create NextPage

---
+++ lib/nextPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget{
+  const NextPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
