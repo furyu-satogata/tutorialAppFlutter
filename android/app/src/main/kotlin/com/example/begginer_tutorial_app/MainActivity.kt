13549e9a4ec694da227cd2c4ee65ec317fa0dbb3
furyu-satogata <satogata.hiromichi@furyu.jp>
2022-11-09 14:13:35 +0900

    enhancement first test print

---
+++ android/app/src/main/kotlin/com/example/begginer_tutorial_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.begginer_tutorial_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tutorial',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Tutorial Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void _goNextPage(){
+    print("test print");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(onPressed: _goNextPage,
+                child:
+            const Text("Next Page"))
+          ],
+        ),
+      ),
+    );
+  }
+}
