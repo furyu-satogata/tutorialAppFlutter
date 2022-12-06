ea9a50b9aa0eab8fd95d811d4940304deb2a64bb
furyu-satogata <satogata.hiromichi@furyu.jp>
2022-12-06 16:26:51 +0900

    メインページを別ファイルにうつした

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:begginer_tutorial_app/nextPage.dart';
+import 'package:begginer_tutorial_app/next_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_translator/google_translator.dart';
+import 'package:begginer_tutorial_app/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,64 +24,5 @@ class MyApp extends StatelessWidget {
     )),
         translateFrom: Locale("ja"),
         translateTo: Locale("en"));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  String _text = '';
-
-  void _detectChangeText(String inputText) {
-    setState(() {
-      _text = inputText;
-    });
-  }
-
-  void _goNextPage(){
-    print("test print");
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => NextPage(title: "NextPage", inputText: _text,))
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(50.0),
-              child: new TextField(
-                enabled: true,
-                maxLength: 100,
-                maxLines: 2,
-                decoration: const InputDecoration(
-                  hintText: '表示したい文字列を入力してください \n (２行目まで表示されます)',
-                  labelText: '文字列',
-                ),
-                onChanged: _detectChangeText,
-              ),
-            ),
-            TextButton(onPressed: _goNextPage,
-                child:
-            const Text("Next Page"))
-          ],
-        ),
-      ),
-    );
   }
 }
---
+++ lib/my_home_page.dart
@@ -0,0 +1,62 @@
+import 'package:begginer_tutorial_app/next_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_translator/google_translator.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String _text = '';
+
+  void _detectChangeText(String inputText) {
+    setState(() {
+      _text = inputText;
+    });
+  }
+
+  void _goNextPage(){
+    print("test print");
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => NextPage(title: "NextPage", inputText: _text,))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(50.0),
+              child: new TextField(
+                enabled: true,
+                maxLength: 100,
+                maxLines: 2,
+                decoration: const InputDecoration(
+                  hintText: '表示したい文字列を入力してください \n (２行目まで表示されます)',
+                  labelText: '文字列',
+                ),
+                onChanged: _detectChangeText,
+              ),
+            ),
+            TextButton(onPressed: _goNextPage,
+                child:
+                const Text("Next Page"))
+          ],
+        ),
+      ),
+    );
+  }
+}
